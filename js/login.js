9403f0f7f7e15914dd436de24e1083eeb93b36dc
Federico de Mello <Federicodmg20@gmail.com>
2022-08-09 21:00:27 -0300

    2

--- js/login.js
+++ js/login.js
@@ -1,23 +1,18 @@
-let forms = document.querySelectorAll(".needs-validation")
 let form = document.getElementById("formulario")
-
-Array.prototype.slice.call(forms).forEach( form => {
-    form.addEventListener("submit", e => {
-        if (!form.checkValidity()) {
-            e.preventDefault()
-            e.stopPropagation()
-        }
-        form.classList.add("was-validated")
-    })
-})
 
 form.addEventListener("submit", e => {
     e.preventDefault()
-    if (email.checkValidity() && contraseña.checkValidity()) {
+    form.classList.add("was-validated")
+    
+    if (!form.checkValidity()) {
+        e.stopPropagation()
+    } else if (form.checkValidity()) {
         localStorage.setItem("logged", true);
         window.location = "index.html"
     }
+
 })
+
 
 
 
