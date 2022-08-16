6f1bec9665d0de005c8a3f00ad34c22b50940bf3
Federico de Mello <Federicodmg20@gmail.com>
2022-08-16 18:50:55 -0300

    ??

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        windows.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-    });
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    windows.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+  });
 });
