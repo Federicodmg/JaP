74debd62f424777225720d421636452bf8ba84ce
Federico de Mello <Federicodmg20@gmail.com>
2022-08-22 14:00:32 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -20,8 +20,8 @@ form.addEventListener("submit", (e) => {
 });
 
 function handleCredentialResponse(response) {
-  let decodificado = response.credential.atob();
-  console.log(decodificado);
-  localStorage.setItem("login", decodificado.name);
+  /*   let decodificado = response.credential.atob(); */
+  console.log(response);
+  /*   localStorage.setItem("login", decodificado.name); */
   window.location = "portada.html";
 }
